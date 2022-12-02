18fd402fe2ca8d874e054b0abb3bc5da583d3623
Tiago Lenza <tiago.lenza@smiledirectclub.com>
2022-12-02 09:13:09 -0300

    Initial Commit

---
+++ iOS-Interview/iOS_InterviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_InterviewApp.swift
+//  iOS-Interview
+//
+//  Created by Tiago Pigatto Lenza on 02/12/22.
+//
+
+import SwiftUI
+
+@main
+struct iOS_InterviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
